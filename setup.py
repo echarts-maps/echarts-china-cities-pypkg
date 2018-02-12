9535022d1a0798b26237e9623ece487fcfd441d1
chfw <wangc_2011@hotmail.com>
2018-02-12 09:31:48 +0000

    :egg: :ferris_wheel: release 0.0.3. no pyecharts dependency hardcoded

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'echarts-china-cities-pypkg'
 AUTHOR = 'C.W.'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'MIT'
 DESCRIPTION = (
     'pyecharts map extension - china cities - python package'
 )
 URL = 'https://github.com/pyecharts/echarts-china-cities-pypkg'
-DOWNLOAD_URL = '%s/archive/0.0.2.tar.gz' % URL
+DOWNLOAD_URL = '%s/archive/0.0.3.tar.gz' % URL
 FILES = ['README.rst',  'CHANGELOG.rst']
 KEYWORDS = [
     'python'
@@ -46,18 +46,20 @@ CLASSIFIERS = [
 ]
 
 INSTALL_REQUIRES = [
+    'lml==0.0.2',
 ]
 SETUP_COMMANDS = install_cmd_for(
     'echarts-china-cities-js', 'echarts_china_cities_pypkg/resources')
 
 
 PACKAGES = find_packages(exclude=['ez_setup', 'examples', 'tests'])
-EXTRAS_REQUIRE = {}
+EXTRAS_REQUIRE = {
+}
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist bdist_wheel upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs echarts-china-cities-pypkg v0.0.2 ' +
-              "Find 0.0.2 in changelog for more details")
+GS_COMMAND = ('gs echarts-china-cities-pypkg v0.0.3 ' +
+              "Find 0.0.3 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = (
